2479ab3acf4c2d299e6ca74e0b2717e827a37bd8
Vishal <mish.vish.fl@gmail.com>
2025-06-06 15:48:13 +0530

    Button added

--- app/src/main/java/com/example/firstclass/MainActivity.java
+++ app/src/main/java/com/example/firstclass/MainActivity.java
@@ -20,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
             //this is updated
+
         });
     }
 }
