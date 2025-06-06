d2802576533fd47885fc913d3eddd0aace361ac5
Vishal <mish.vish.fl@gmail.com>
2025-06-06 16:20:21 +0530

    feature is updated

--- app/src/main/java/com/example/firstclass/MainActivity.java
+++ app/src/main/java/com/example/firstclass/MainActivity.java
@@ -19,7 +19,8 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-            //this is updated
+
+            //this is feature-team.
 
         });
     }
